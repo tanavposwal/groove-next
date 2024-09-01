b4a99beb7103f26b609d6882341dbd94aeb068bc
Tanav Poswal <tanavposwal939@gmail.com>
2024-09-01 16:16:30 +0530

    ui fix 2

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,13 +3,23 @@
 import { Button } from "./ui/button";
 import { useRecoilState } from "recoil";
 import { idState } from "../state/state";
+import { ArrowBigDown, ArrowBigUp, Trash2 } from "lucide-react";
 
 export default function Player({ id }: { id: string }) {
   const [ytid, setYtId] = useRecoilState(idState);
   return (
-    <div>
+    <div className="flex gap-2 items-center">
       <Button variant="outline" size="sm" onClick={() => {setYtId(id)}}>
         Play
+      </Button>
+      <Button variant="ghost" size="icon">
+        <ArrowBigUp className="scale-90" />
+      </Button>
+      <Button variant="ghost" size="icon">
+        <ArrowBigDown className="scale-90" />
+      </Button>
+      <Button variant="ghost" size="icon">
+        <Trash2 className="scale-75" />
       </Button>
     </div>
   );
