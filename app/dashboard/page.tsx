8eef8f5e4846eab2405076704de880670b903b55
Tanav Poswal <tanavposwal939@gmail.com>
2024-09-01 13:04:51 +0530

    ui fixed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,16 +5,18 @@ import Navbar from "@/components/Navbar";
 import Queue from "@/components/Queue";
 
 export default async function Dashboard() {
-    const session = await auth()
+  const session = await auth();
 
-    return (
-        <main>
-            <Navbar />
-            <section className="flex gap-3 max-w-screen-xl mx-auto flex-col md:felx-row p-4">
-            <Adder userID={session?.user?.id!} />
-            <MiniWindows />
-            <Queue />
-            </section>
-        </main>
-    )
+  return (
+    <main>
+      <Navbar />
+      <section className="flex gap-3 max-w-screen-xl mx-auto flex-col md:flex-row p-4">
+        <Queue />
+        <div className="flex flex-col gap-3">
+          <Adder userID={session?.user?.id!} />
+          <MiniWindows />
+        </div>
+      </section>
+    </main>
+  );
 }
