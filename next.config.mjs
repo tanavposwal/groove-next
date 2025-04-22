158d66083c722d67cb50fb7c840139ed763865c2
tanavposwal <tanavposwal939@gmail.com>
2025-04-22 20:17:51 +0530

    config clean

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['i.ytimg.com', 'img.youtube.com'],
-    },
-    experimental: {
-        serverActions: true,
-    },
-    poweredByHeader: false,
-    compress: true,
-    reactStrictMode: true,
-    swcMinify: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
